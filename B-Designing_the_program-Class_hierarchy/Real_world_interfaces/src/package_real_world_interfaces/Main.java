fc79e81f3137bc51f134e3302e48519d6dd33645
Luis Miguel Barrera Romero <lmbr@protonmail.com>
2025-01-26 20:43:00 +0100

    real world interface

--- B-Designing_the_program-Class_hierarchy/Real_world_interfaces/src/package_real_world_interfaces/Main.java
+++ B-Designing_the_program-Class_hierarchy/Real_world_interfaces/src/package_real_world_interfaces/Main.java
@@ -1,10 +1,16 @@
 package package_real_world_interfaces;
 
 public class Main {
-	//Declare MusicPlayer object
-	MusicPlayer m = new MusicPlayer();
-	//start playing a song
-	
-	//adjust volume
-//	player.adjustVolume(50);
+	public static void main(String[] args) {
+		//Declare MusicPlayer object
+		MusicPlayer m = new MusicPlayer();
+		//start playing a song
+		m.play("Life misght be a Deep Fake");
+		// Adjust volume
+		m.adjustVolume(50);
+		// pause the player
+		m.pause();
+		// stop playing
+		m.stop();
+	}
 }
